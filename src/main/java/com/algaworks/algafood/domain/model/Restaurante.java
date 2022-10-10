9d27b656a78383006d8a0be39acb335fc9b8e415
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-10 09:03:55 -0300

    Mapeando o id da entidade para autoincremento

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.domain.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Objects;
@@ -10,6 +12,7 @@ import java.util.Objects;
 public class Restaurante {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
