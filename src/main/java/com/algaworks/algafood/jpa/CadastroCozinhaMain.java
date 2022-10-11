cd58325a81b6fcca27f14c4cf0cb26648a61eacd
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-11 06:36:26 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinhaMain.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.jpa;
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class CadastroCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        cozinhas.forEach(c -> System.out.println(c.getNome()));
+    }
+}
