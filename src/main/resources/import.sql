03efdff06dea3018cf7abd344418bb57cdee7420
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-12 11:50:56 -0300

    Atualizando import de dados conforme o Curso

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,24 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
 
-insert into forma_pagamento (descricao) values ('Dinheiro');
-insert into forma_pagamento (descricao) values ('Cartão de Crédito');
-insert into forma_pagamento (descricao) values ('Cartão de Débito');
+ insert into estado (id, nome) values (1, 'Minas Gerais');
+ insert into estado (id, nome) values (2, 'São Paulo');
+ insert into estado (id, nome) values (3, 'Ceará');
 
-insert into permissao (nome, descricao) values ('Cadastrar Restaurante', 'Permite realizar o cadastro de restaurantes');
-insert into permissao (nome, descricao) values ('Cadastrar Cozinha', 'Permite realizar o cadastro de cozinhas');
+ insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
+ insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
+ insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
+ insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
+ insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
-insert into estado (id, nome) values (1, 'Paraná');
-insert into estado (id, nome) values (2, 'São Paulo');
-insert into estado (id, nome) values (3, 'Minas Gerais');
+ insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+ insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
-insert into cidade (nome, estado_id) values ('Campo Mourão', 1);
-insert into cidade (nome, estado_id) values ('São Paulo', 2);
-insert into cidade (nome, estado_id) values ('Belo Horizonte', 3);
+ insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+ insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
