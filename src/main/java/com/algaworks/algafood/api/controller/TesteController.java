83fc8c5d679d984a86012c4550dbba525fb40da0
Marcos F. Alves <marcosf_alves@outlook.com>
2022-10-22 10:38:38 -0300

    Criando consultas com query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -16,8 +17,13 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-//    @GetMapping("/cozinhas/por-nome")
-//    public List<Cozinha> buscarPorNome(@RequestParam String nome){
-//        return cozinhaRepository.buscarPorNome(nome);
-//    }
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(@RequestParam String nome){
+        return cozinhaRepository.findAllByNome(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome){
+        return cozinhaRepository.findOneByNome(nome);
+    }
 }
