feb26f93e30591b1bc265b7421d8c331954cdca5
Marcos F. Alves <marcosf_alves@outlook.com>
2022-11-02 09:20:57 -0300

    Implementando uma consulta simples com Criteria API

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
@@ -21,27 +23,12 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
 
-        var jpql = new StringBuilder("from Restaurante where 0 = 0 ");
-        var parametros = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-        if (StringUtils.hasLength(nome)){
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
-        }
+        CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+        criteria.from(Restaurante.class);
 
-        if (taxaFreteInicial != null){
-            jpql.append("and taxaFrete >= :taxaInicial ");
-            parametros.put("taxaInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null){
-            jpql.append("and taxaFrete <= :taxaFinal ");
-            parametros.put("taxaFinal", taxaFreteFinal);
-        }
-
-        TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+        TypedQuery<Restaurante> query = manager.createQuery(criteria);
 
         return query.getResultList();
     }
