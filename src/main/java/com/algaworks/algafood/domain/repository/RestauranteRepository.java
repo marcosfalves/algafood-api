409523fe79d1ac44203bca3882057efb7e183449
Marcos F. Alves <marcosf_alves@outlook.com>
2022-11-13 18:59:16 -0300

    Ajustando collection resource de restaurante

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries,
         JpaSpecificationExecutor<Restaurante> {
 
-    @Query("select distinct r from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("select r from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
     List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
