3c23d8632f7245a9bdc06b3037fc50fd548a6615
Marcos F. Alves <marcosf_alves@outlook.com>
2022-12-23 07:23:49 -0300

    Removendo mixin de Restaurante que não está mais sendo utilizado

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -4,19 +4,16 @@ import com.algaworks.algafood.api.model.mixin.CidadeMixin;
 import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.ItemPedidoMixin;
 import com.algaworks.algafood.api.model.mixin.PedidoMixin;
-import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.ItemPedido;
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JacksonMixinModule extends SimpleModule {
     public JacksonMixinModule() {
-        setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
         setMixInAnnotation(Cidade.class, CidadeMixin.class);
         setMixInAnnotation(Cozinha.class, CozinhaMixin.class);
         setMixInAnnotation(Pedido.class, PedidoMixin.class);
