1b849bc76bd13402c3099ee9b33e56e6bc49084a
Marcos F. Alves <marcosf_alves@outlook.com>
2022-12-29 09:08:12 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.core.modelmapper;
 
+import com.algaworks.algafood.api.model.CidadeResumoModel;
+import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.domain.model.Cidade;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Cidade.class, CidadeResumoModel.class)
+                .<String>addMapping(
+                        cidadeSrc -> cidadeSrc.getEstado().getNome(),
+                        (cidadeResumoModelDest, value) -> cidadeResumoModelDest.setEstado(value)
+                );
+
+        return modelMapper;
     }
 }
