1de03ab204809fbc41d29e91521e9eae3452a7c5
Marcos F. Alves <marcosf_alves@outlook.com>
2022-12-30 08:29:43 -0300

    Desafio: implementando os endpoints de grupos

---
+++ src/main/java/com/algaworks/algafood/domain/exception/GrupoNaoEncontradoException.java
@@ -0,0 +1,11 @@
+package com.algaworks.algafood.domain.exception;
+
+public class GrupoNaoEncontradoException extends EntidadeNaoEncontradaException {
+    public GrupoNaoEncontradoException(String mensagem){
+        super(mensagem);
+    }
+
+    public GrupoNaoEncontradoException(Long grupoId) {
+        this(String.format("Não existe um cadastro de grupo com código %d", grupoId));
+    }
+}
