186bef7dfd1e657b997947f8050d4fd3c2172d92
Marcos F. Alves <marcosf_alves@outlook.com>
2022-12-31 08:53:01 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package com.algaworks.algafood.domain.repository;
 import com.algaworks.algafood.domain.model.Usuario;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.service;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.SenhaInvalidaException;
 import com.algaworks.algafood.domain.exception.UsuarioNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Usuario;
@@ -7,6 +8,8 @@ import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class CadastroUsuarioService {
@@ -16,6 +19,16 @@ public class CadastroUsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
+        usuarioRepository.detach(usuario);
+
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)){
+            throw new NegocioException(
+              String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail())
+            );
+        }
+
         return usuarioRepository.save(usuario);
     }
 
