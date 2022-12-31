9efaced1e4b152820d8697b7656e1aea9aa530ac
Marcos F. Alves <marcosf_alves@outlook.com>
2022-12-31 16:30:11 -0300

    Implementando os endpoints de associação de formas de pagamento em restaurantes

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -19,7 +19,9 @@ import javax.persistence.OneToMany;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -58,7 +60,7 @@ public class Restaurante {
     @JoinTable(name = "restaurante_forma_pagamento",
             joinColumns = @JoinColumn(name = "restaurante_id"),
             inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-    private List<FormaPagamento> formasPagamento = new ArrayList<>();
+    private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
@@ -69,5 +71,13 @@ public class Restaurante {
 
     public void inativar(){
         setAtivo(false);
+    }
+
+    public boolean adicionarFormaPagamento(FormaPagamento formaPagamento){
+        return this.formasPagamento.add(formaPagamento);
+    }
+
+    public boolean removerFormaPagamento(FormaPagamento formaPagamento){
+        return this.formasPagamento.remove(formaPagamento);
     }
 }
