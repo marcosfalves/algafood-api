4076653a540afbaedb0380d2cbf07fa834d59c54
Marcos F. Alves <marcosf_alves@outlook.com>
2023-01-02 19:35:02 -0300

    Desafio: implementando os endpoints de associação de grupos com permissões

--- src/main/java/com/algaworks/algafood/domain/model/Grupo.java
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -12,7 +12,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -31,5 +33,13 @@ public class Grupo {
     @JoinTable(name = "grupo_permissao",
             joinColumns = @JoinColumn(name = "grupo_id"),
             inverseJoinColumns = @JoinColumn(name = "permissao_id"))
-    private List<Permissao> permissoes = new ArrayList<>();
+    private Set<Permissao> permissoes = new HashSet<>();
+
+    public boolean adicionarPermissao(Permissao permissao){
+        return permissoes.add(permissao);
+    }
+
+    public boolean removerPermissao(Permissao permissao) {
+        return permissoes.remove(permissao);
+    }
 }
