1553f2924b0c16ec90210b38ab23371f6208a0a7
Marcos F. Alves <marcosf_alves@outlook.com>
2023-01-16 09:05:22 -0300

    Implementando a recuperação de foto no serviço de storage do S3

--- src/main/java/com/algaworks/algafood/infrastructure/storage/LocalFotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/storage/LocalFotoStorageService.java
@@ -3,10 +3,8 @@ package com.algaworks.algafood.infrastructure.storage;
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -17,11 +15,13 @@ public class LocalFotoStorageService implements FotoStorageService {
     private StorageProperties storageProperties;
 
     @Override
-    public InputStream recuperar(String nomeArquivo) {
+    public FotoRecuperada recuperar(String nomeArquivo) {
         try {
             Path arquivoPath = getArquivoPath(nomeArquivo);
 
-            return Files.newInputStream(arquivoPath);
+            return FotoRecuperada.builder()
+                    .inputStream(Files.newInputStream(arquivoPath))
+                    .build();
         } catch (Exception e) {
             throw new StorageException("Não foi possível recuperar arquivo.", e);
         }
