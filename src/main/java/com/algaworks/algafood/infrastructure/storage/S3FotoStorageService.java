f2d34560fcf384bc5ab4d034e28c035bbf932141
Marcos F. Alves <marcosf_alves@outlook.com>
2023-01-16 08:40:55 -0300

    Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infrastructure/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/storage/S3FotoStorageService.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,14 +33,14 @@ public class S3FotoStorageService implements FotoStorageService {
             var objectMetadata = new ObjectMetadata();
             objectMetadata.setContentType(novaFoto.getContentType());
 
-            var putObjectResquest = new PutObjectRequest(
+            var putObjectRequest = new PutObjectRequest(
                     storageProperties.getS3().getBucket(),
                     caminhoArquivo,
                     novaFoto.getInputStream(),
                     objectMetadata
             ).withCannedAcl(CannedAccessControlList.PublicRead);
 
-            amazonS3.putObject(putObjectResquest);
+            amazonS3.putObject(putObjectRequest);
         } catch (Exception e) {
             throw new StorageException("Não foi possível enviar arquivo para Amazon S3.", e);
         }
@@ -51,6 +52,16 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+        try {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
 
+            var deleteObjectRequest = new DeleteObjectRequest(
+                    storageProperties.getS3().getBucket(),
+                    caminhoArquivo);
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+        }
     }
 }
