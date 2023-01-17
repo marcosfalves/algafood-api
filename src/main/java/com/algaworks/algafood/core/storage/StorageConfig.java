c2e1dcb3fcb01c356a51f7b4d4bb260c32e2be62
Marcos F. Alves <marcosf_alves@outlook.com>
2023-01-17 07:31:59 -0300

    Selecionando a implementação do serviço de storage de fotos

--- src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
+++ src/main/java/com/algaworks/algafood/core/storage/StorageConfig.java
@@ -1,21 +1,27 @@
 package com.algaworks.algafood.core.storage;
 
+import com.algaworks.algafood.core.storage.StorageProperties.TipoStorage;
+import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.algaworks.algafood.infrastructure.storage.LocalFotoStorageService;
+import com.algaworks.algafood.infrastructure.storage.S3FotoStorageService;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AmazonS3Config {
+public class StorageConfig {
 
     @Autowired
     private StorageProperties storageProperties;
 
     @Bean
-    public AmazonS3 amazonS3(){
+    @ConditionalOnProperty(name = "algafood.storage.tipo", havingValue = "s3")
+    public AmazonS3 amazonS3() {
         var credentials = new BasicAWSCredentials(
                 storageProperties.getS3().getIdChaveAcesso(),
                 storageProperties.getS3().getChaveAcessoSecreta());
@@ -24,5 +30,14 @@ public class AmazonS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(storageProperties.getS3().getRegiao())
                 .build();
+    }
+
+    @Bean
+    public FotoStorageService fotoStorageService() {
+        if (storageProperties.getTipo() == TipoStorage.S3) {
+            return new S3FotoStorageService();
+        } else {
+            return new LocalFotoStorageService();
+        }
     }
 }
