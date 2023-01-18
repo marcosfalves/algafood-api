313cd416fb360991ab1a41b1f006e80b443b181f
Marcos F. Alves <marcosf_alves@outlook.com>
2023-01-18 07:24:11 -0300

    Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EnvioEmailService.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.domain.service;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.Singular;
 
 import java.util.Set;
 
@@ -11,8 +13,13 @@ public interface EnvioEmailService {
     @Builder
     @Getter
     class Mensagem {
+        @Singular
         private Set<String> destinatarios;
+
+        @NonNull
         private String assunto;
+
+        @NonNull
         private String corpo;
     }
 }
--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.service.EnvioEmailService.Mensagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,10 +12,21 @@ public class FluxoPedidoService {
     @Autowired
     private EmissaoPedidoService emissaoPedido;
 
+    @Autowired
+    private EnvioEmailService envioEmail;
+
     @Transactional
     public void confirmar(String codigoPedido) {
         Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
         pedido.confirmar();
+
+        var mensagem = Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmail.enviar(mensagem);
     }
 
     @Transactional
