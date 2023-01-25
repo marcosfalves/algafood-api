b66d27b56a65fe019ef4fa3fa1e29878e83b5994
Marcos F. Alves <31605679+marcosfalves@users.noreply.github.com>
2023-01-25 08:39:27 -0300

    Módulo 16 - CORS e consumo da API com JavaScript e Java (#6)

---
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
+//                .maxAge(10) //Para testar com preflight
+    }
+}
