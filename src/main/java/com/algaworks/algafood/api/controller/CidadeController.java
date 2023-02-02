11af1c26a5bc6d1fffdab6327dd0f7d99eb8bc56
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-02 09:00:42 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.assembler.CidadeModelAssembler;
+import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -12,8 +13,13 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -52,6 +58,13 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "ID da cidade é inválido",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,  schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,  schema = @Schema(implementation = Problem.class)))
+
+    })
     @GetMapping("/{cidadeId}")
     public CidadeModel buscar(@ApiParam("ID de uma cidade")
                               @PathVariable Long cidadeId) {
@@ -61,6 +74,9 @@ public class CidadeController {
     }
 
     @ApiOperation("Cadastra uma cidade")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cidade cadastrada")
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
@@ -76,6 +92,12 @@ public class CidadeController {
     }
 
     @ApiOperation("Atualiza uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cidade atualizada"),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,  schema = @Schema(implementation = Problem.class)))
+
+    })
     @PutMapping("/{cidadeId}")
     public CidadeModel atualizar(@ApiParam("ID de uma cidade")
                                  @PathVariable Long cidadeId,
@@ -95,6 +117,12 @@ public class CidadeController {
     }
 
     @ApiOperation("Exclui uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Cidade excluída"),
+            @ApiResponse(responseCode = "404", description = "Cidade não encontrada",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,  schema = @Schema(implementation = Problem.class)))
+
+    })
     @DeleteMapping("{cidadeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@ApiParam("ID de uma cidade")
