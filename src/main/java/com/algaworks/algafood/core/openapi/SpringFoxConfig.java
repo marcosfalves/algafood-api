ae897376fd9ce9ca0ca4bc591edfe5f63ee12355
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-02 06:55:11 -0300

    Refatorando construção das mensagens de erro globais

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -17,11 +17,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
+
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.OAS_30)
@@ -31,58 +35,11 @@ public class SpringFoxConfig {
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponses(HttpMethod.GET, globalGetResponseMessages())
-                .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
-                .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.POST, globalPostResponseMessages())
+                .globalResponses(HttpMethod.PUT, globalPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as Cidades"));
-    }
-
-    private List<Response> globalGetResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                        .description("Erro interno no servidor")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-                        .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-                        .build()
-        );
-    }
-
-    private List<Response> globalPostPutResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                        .description("Requisição inválida (erro do cliente)")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                        .description("Erro interno no servidor")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
-                        .description("Recurso não possui representação que poderia ser aceita pelo consumidor")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
-                        .description("Requisição recusada porque o corpo está em um formato não suportado")
-                        .build()
-        );
-    }
-
-    private List<Response> globalDeleteResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
-                        .description("Requisição inválida (erro do cliente)")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                        .description("Erro interno no servidor")
-                        .build()
-        );
     }
 
     private ApiInfo apiInfo() {
@@ -92,5 +49,59 @@ public class SpringFoxConfig {
                 .version("1")
                 .contact(new Contact("Marcos F. Alves", "https://github.com/marcosfalves", "marcosf_alves@outlook.com"))
                 .build();
+    }
+
+    private List<Response> globalGetResponseMessages() {
+        return buildResponseMessagesHttpStatusAccepted(
+                Arrays.asList(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        HttpStatus.NOT_ACCEPTABLE
+                ));
+    }
+
+    private List<Response> globalPostResponseMessages() {
+        return buildResponseMessagesHttpStatusAccepted(
+                Arrays.asList(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.NOT_ACCEPTABLE,
+                        HttpStatus.UNSUPPORTED_MEDIA_TYPE
+                ));
+    }
+
+    private List<Response> globalPutResponseMessages() {
+        return buildResponseMessagesHttpStatusAccepted(
+                Arrays.asList(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.NOT_ACCEPTABLE,
+                        HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+                        HttpStatus.NOT_FOUND
+                ));
+    }
+
+    private List<Response> globalDeleteResponseMessages() {
+        return buildResponseMessagesHttpStatusAccepted(
+                Arrays.asList(
+                        HttpStatus.INTERNAL_SERVER_ERROR,
+                        HttpStatus.BAD_REQUEST,
+                        HttpStatus.NOT_FOUND
+                ));
+    }
+
+    private List<Response> buildResponseMessagesHttpStatusAccepted(List<HttpStatus> httpStatusAccepted) {
+        Map<HttpStatus, String> messages = new HashMap<>();
+        messages.put(HttpStatus.INTERNAL_SERVER_ERROR, "Erro interno no servidor");
+        messages.put(HttpStatus.BAD_REQUEST, "Requisição inválida (erro do cliente)");
+        messages.put(HttpStatus.NOT_FOUND, "Recurso não encontrado");
+        messages.put(HttpStatus.NOT_ACCEPTABLE, "Recurso não possui representação que poderia ser aceita pelo consumidor");
+        messages.put(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "Requisição recusada porque o corpo está em um formato não suportado");
+
+        return httpStatusAccepted.stream()
+                .map(httpStatus -> new ResponseBuilder()
+                        .code(String.valueOf(httpStatus.value()))
+                        .description(messages.get(httpStatus))
+                        .build())
+                .collect(Collectors.toList());
     }
 }
