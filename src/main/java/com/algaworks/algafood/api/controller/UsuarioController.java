a84defeeb5800cdabcfef67450c76ad430c97132
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-11 09:31:06 -0300

    Desafio: descrevendo documentação de endpoints de usuários

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.algaworks.algafood.api.model.UsuarioModel;
 import com.algaworks.algafood.api.model.input.SenhaInput;
 import com.algaworks.algafood.api.model.input.UsuarioComSenhaInput;
 import com.algaworks.algafood.api.model.input.UsuarioInput;
+import com.algaworks.algafood.api.openapi.controller.UsuarioControllerOpenApi;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
@@ -26,7 +27,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/usuarios", produces = MediaType.APPLICATION_JSON_VALUE)
-public class UsuarioController {
+public class UsuarioController implements UsuarioControllerOpenApi {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -66,7 +67,7 @@ public class UsuarioController {
 
     @PutMapping("/{usuarioId}")
     public UsuarioModel atualizar(@PathVariable Long usuarioId,
-                                @RequestBody @Valid UsuarioInput usuarioInput) {
+                                  @RequestBody @Valid UsuarioInput usuarioInput) {
         Usuario usuarioAtual = cadastroUsuario.buscarOuFalhar(usuarioId);
 
         usuarioInputDisassembler.copyToDomainObject(usuarioInput, usuarioAtual);
--- src/main/java/com/algaworks/algafood/api/model/input/SenhaInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/SenhaInput.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model.input;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,12 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class SenhaInput {
+
+    @ApiModelProperty(example = "123", required = true)
     @NotBlank
     private String senhaAtual;
 
+    @ApiModelProperty(example = "123", required = true)
     @NotBlank
     private String novaSenha;
 }
