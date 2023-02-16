a63bb86d2d9d121958cc5b491bff885bfba0f762
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-16 06:16:00 -0300

    Desafio: adicionando hypermedia nos recursos de estados

--- src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/EstadoModel.java
@@ -4,10 +4,12 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
+@Relation(collectionRelation = "estados")
 @Getter
 @Setter
-public class EstadoModel extends RepresentationModel {
+public class EstadoModel extends RepresentationModel<EstadoModel> {
 
     @ApiModelProperty(example = "1")
     private Long id;
