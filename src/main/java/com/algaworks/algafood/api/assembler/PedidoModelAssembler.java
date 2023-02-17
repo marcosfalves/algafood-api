e2da67ccffd8743a99e79c6a772e8f225406ace2
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-17 17:58:02 -0300

    Desafio: adicionando template variables do filtro de pedidos

--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -40,13 +40,23 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 //        pedidoModel.add(
 //          linkTo(PedidoController.class).withRel(IanaLinkRelations.COLLECTION)
 //        );
-        TemplateVariables pageVariables = new TemplateVariables(
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+
+        var pageVariables = new TemplateVariables(
                 new TemplateVariable("page", VariableType.REQUEST_PARAM),
                 new TemplateVariable("size", VariableType.REQUEST_PARAM),
                 new TemplateVariable("sort", VariableType.REQUEST_PARAM)
         );
-        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl, pageVariables), IanaLinkRelations.COLLECTION));
+
+        var filtroVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM)
+        );
+
+        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,
+                pageVariables.concat(filtroVariables)), IanaLinkRelations.COLLECTION));
 
         pedidoModel.getRestaurante().add(linkTo(
                         methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())
