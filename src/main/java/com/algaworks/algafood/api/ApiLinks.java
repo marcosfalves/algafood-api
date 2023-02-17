996aabeca21fcebbbe8256a8a8a7251398b218db
Marcos F. Alves <marcosf_alves@outlook.com>
2023-02-17 18:09:45 -0300

    Refatorando construção e inclusão de links em representation model

---
+++ src/main/java/com/algaworks/algafood/api/ApiLinks.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.api;
+
+import com.algaworks.algafood.api.controller.PedidoController;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@Component
+public class ApiLinks {
+
+    public static final TemplateVariables PAGINACAO_VARIABLES = new TemplateVariables(
+            new TemplateVariable("page", VariableType.REQUEST_PARAM),
+            new TemplateVariable("size", VariableType.REQUEST_PARAM),
+            new TemplateVariable("sort", VariableType.REQUEST_PARAM)
+    );
+
+    public Link linkToPedidos() {
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+
+        var filtroVariables = new TemplateVariables(
+                new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM)
+        );
+
+        return Link.of(UriTemplate.of(pedidosUrl,
+                PAGINACAO_VARIABLES.concat(filtroVariables)), IanaLinkRelations.COLLECTION);
+    }
+}
--- src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/PedidoModelAssembler.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.assembler;
 
+import com.algaworks.algafood.api.ApiLinks;
 import com.algaworks.algafood.api.controller.CidadeController;
 import com.algaworks.algafood.api.controller.FormaPagamentoController;
 import com.algaworks.algafood.api.controller.PedidoController;
@@ -10,12 +11,6 @@ import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.IanaLinkRelations;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.TemplateVariable;
-import org.springframework.hateoas.TemplateVariable.VariableType;
-import org.springframework.hateoas.TemplateVariables;
-import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -28,6 +23,9 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private ApiLinks apiLinks;
+
     public PedidoModelAssembler() {
         super(PedidoController.class, PedidoModel.class);
     }
@@ -37,26 +35,7 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
         var pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModel);
 
-//        pedidoModel.add(
-//          linkTo(PedidoController.class).withRel(IanaLinkRelations.COLLECTION)
-//        );
-        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
-
-        var pageVariables = new TemplateVariables(
-                new TemplateVariable("page", VariableType.REQUEST_PARAM),
-                new TemplateVariable("size", VariableType.REQUEST_PARAM),
-                new TemplateVariable("sort", VariableType.REQUEST_PARAM)
-        );
-
-        var filtroVariables = new TemplateVariables(
-                new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
-                new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
-                new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
-                new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM)
-        );
-
-        pedidoModel.add(Link.of(UriTemplate.of(pedidosUrl,
-                pageVariables.concat(filtroVariables)), IanaLinkRelations.COLLECTION));
+        pedidoModel.add(apiLinks.linkToPedidos());
 
         pedidoModel.getRestaurante().add(linkTo(
                         methodOn(RestauranteController.class).buscar(pedido.getRestaurante().getId())
