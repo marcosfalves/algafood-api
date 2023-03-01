9df4ef3d3bdfe8840da82d436543122b5e96afe2
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-01 08:22:53 -0300

    Configurando o Cors para fluxo  Authorization Code

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -14,6 +14,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .anyRequest().authenticated()
              .and()
+             .cors().and()
              .oauth2ResourceServer()
                 .opaqueToken();
     }
