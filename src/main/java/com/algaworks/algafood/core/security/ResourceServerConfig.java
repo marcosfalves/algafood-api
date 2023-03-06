6c1adc57a0ac3ba5bbd5364358ccdcd6a87f5bab
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-06 09:02:33 -0300

    Configuração para não criar cookie de sessão

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +14,9 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
+             .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
              .and()
              .cors().and()
              .oauth2ResourceServer()
