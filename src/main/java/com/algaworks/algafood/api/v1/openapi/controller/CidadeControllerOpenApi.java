301eb82ff33101e6a3e5165feef0a59e5a67f643
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-24 06:01:40 -0300

    Descrevendo códigos de status de respostas em endpoints específicos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -4,7 +4,10 @@ import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -15,7 +18,12 @@ public interface CidadeControllerOpenApi {
     @Operation(summary = "Lista as cidades")
     CollectionModel<CidadeModel> listar();
 
-    @Operation(summary = "Busca uma cidade por ID")
+    @Operation(summary = "Busca uma cidade por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "400", description = "ID da cidade é inválido",
+                            content = @Content(schema = @Schema))
+            })
     CidadeModel buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
     @Operation(summary = "Cadastra uma cidade",
