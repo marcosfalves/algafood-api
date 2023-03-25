f87f32780d8d20c5e7c0c253d6dc489b018a556c
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-25 08:01:20 -0300

    Corrigindo documentação com substituição de Pageable

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -2,13 +2,16 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 
 import com.algaworks.algafood.api.v1.model.CozinhaModel;
 import com.algaworks.algafood.api.v1.model.input.CozinhaInput;
+import com.algaworks.algafood.core.springdoc.PageableParameter;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
 
 @SecurityRequirement(name = "security_auth")
 public interface CozinhaControllerOpenApi {
-    PagedModel<CozinhaModel> listar(Pageable pageable);
+    @PageableParameter
+    PagedModel<CozinhaModel> listar(@Parameter(hidden = true) Pageable pageable);
 
     CozinhaModel buscar(Long cozinhaId);
 
