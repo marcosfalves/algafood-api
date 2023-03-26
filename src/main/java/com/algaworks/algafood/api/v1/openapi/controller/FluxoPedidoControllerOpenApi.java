04a751b208082532c716c820db4cb3780affa4bc
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-26 10:21:36 -0300

    Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FluxoPedidoControllerOpenApi.java
@@ -1,14 +1,34 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Pedidos")
 @SecurityRequirement(name = "security_auth")
 public interface FluxoPedidoControllerOpenApi {
 
-    ResponseEntity<Void> confirmar(String codigoPedido);
+    @Operation(summary = "Confirmação de pedido",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Pedido confirmado com sucesso")
+            }
+    )
+    ResponseEntity<Void> confirmar(@Parameter(description = "Código do pedido", example = "b5741512-8fbc-47fa-9ac1-b530354fc0ff") String codigoPedido);
 
-    ResponseEntity<Void> entregar(String codigoPedido);
+    @Operation(summary = "Registrar entrega de pedido",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Entrega de pedido registrada com sucesso")
+            }
+    )
+    ResponseEntity<Void> entregar(@Parameter(description = "Código do pedido", example = "b5741512-8fbc-47fa-9ac1-b530354fc0ff") String codigoPedido);
 
-    ResponseEntity<Void> cancelar(String codigoPedido);
+    @Operation(summary = "Cancelamento de pedido",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Pedido cancelado com sucesso")
+            }
+    )
+    ResponseEntity<Void> cancelar(@Parameter(description = "Código do pedido", example = "b5741512-8fbc-47fa-9ac1-b530354fc0ff") String codigoPedido);
 }
