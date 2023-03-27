72cca6689febaf4ed2f279fd8e3bc94d46adefc5
Marcos F. Alves <marcosf_alves@outlook.com>
2023-03-27 06:26:38 -0300

    Desafio: descrevendo documentação de endpoints de associação de permissões com grupos

--- src/main/java/com/algaworks/algafood/api/v1/model/PermissaoModel.java
+++ src/main/java/com/algaworks/algafood/api/v1/model/PermissaoModel.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.v1.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -9,10 +10,12 @@ import org.springframework.hateoas.server.core.Relation;
 @Getter
 @Setter
 public class PermissaoModel extends RepresentationModel<PermissaoModel> {
-
+    @Schema(example = "1")
     private Long id;
 
+    @Schema(example = "EDITAR_COZINHAS")
     private String nome;
 
+    @Schema(example = "Permite editar cozinhas")
     private String descricao;
 }
--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoPermissaoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/GrupoPermissaoControllerOpenApi.java
@@ -1,16 +1,44 @@
 package com.algaworks.algafood.api.v1.openapi.controller;
 
 import com.algaworks.algafood.api.v1.model.PermissaoModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Grupos")
 @SecurityRequirement(name = "security_auth")
 public interface GrupoPermissaoControllerOpenApi {
 
-    CollectionModel<PermissaoModel> listar(Long grupoId);
+    @Operation(summary = "Lista as permissões associadas a um grupo",
+            responses = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "400", description = "ID do grupo é inválido",
+                            content = @Content(schema = @Schema(ref = "Problema"))),
+                    @ApiResponse(responseCode = "404", description = "Grupo não encontrado",
+                            content = @Content(schema = @Schema(ref = "Problema")))
+            }
+    )
+    CollectionModel<PermissaoModel> listar(@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId);
 
-    ResponseEntity<Void> associarPermissao(Long grupoId, Long permissaoId);
+    @Operation(summary = "Associação de permissão com grupo",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Associação realizada com sucesso")
+            }
+    )
+    ResponseEntity<Void> associarPermissao(@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId,
+                                           @Parameter(description = "ID da permissão", example = "1", required = true) Long permissaoId);
 
-    ResponseEntity<Void> desassociarPermissao(Long grupoId, Long permissaoId);
+    @Operation(summary = "Desassociação de permissão com grupo",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso")
+            }
+    )
+    ResponseEntity<Void> desassociarPermissao(@Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId,
+                                              @Parameter(description = "ID da permissão", example = "1", required = true) Long permissaoId);
 }
